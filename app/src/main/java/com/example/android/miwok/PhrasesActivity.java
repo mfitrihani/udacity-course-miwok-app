e58af30a9e50a6ade7f7d70303b0957c05cc330e
mfitrihani <mfitrihani@gmail.com>
2019-10-29 16:18:06 +0800

    initial commit

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,8 +15,9 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class PhrasesActivity extends AppCompatActivity {
 
