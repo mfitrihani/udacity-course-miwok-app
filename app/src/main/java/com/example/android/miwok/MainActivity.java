2dd6585d14e14ad527e3988ede968c123ed60d4a
mfitrihani <mfitrihani@gmail.com>
2019-10-31 14:13:46 +0800

    add function that add text view dynamically to number Layout

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,7 +19,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,26 +15,43 @@
  */
 package com.example.android.miwok;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set content view
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] ="one";
-        words[1] ="two";
-        words[2] ="three";
-        words[3] ="four";
-        words[4] ="five";
-        words[5] ="six";
-        words[6] ="seven";
-        words[7] ="eight";
-        words[8] ="nine";
-        words[9] ="ten";
+        //create list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+
+        //rootView object
+        LinearLayout rootView = findViewById(R.id.root_view);
+
+        //add every word as textView and add into root view
+        for (int index=0;index<words.size();index++){
+            TextView newTextView = new TextView(this);
+            newTextView.setText(words.get(index));
+            rootView.addView(newTextView);
+            index++;
+        }
+
+
     }
 }
