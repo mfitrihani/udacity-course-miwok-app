d96d39340d74c9779e345c647102ba979b2abc6c
mfitrihani <mfitrihani@gmail.com>
2019-11-11 15:40:03 +0800

    add new constructor for audio files minor list_item edit

--- app/src/main/java/com/example/android/miwok/word.java
+++ app/src/main/java/com/example/android/miwok/word.java
@@ -5,7 +5,15 @@ public class word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageAssetId = no_image_value;
+    private int audioAssetId = no_image_value;
     private final static int no_image_value = -1;
+
+    public word(String mDefaultTranslation, String mMiwokTranslation, int mImageAssetId, int audioAssetId) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mImageAssetId = mImageAssetId;
+        this.audioAssetId = audioAssetId;
+    }
 
     public word(String defaultTranslation, String miwokTranslation, int imageAssetId){
         mImageAssetId = imageAssetId;
@@ -34,4 +42,6 @@ public class word {
         }
         else return true;
     }
+
+    public int getAudioAssetId(){ return audioAssetId; }
 }
